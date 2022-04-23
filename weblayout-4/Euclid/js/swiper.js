c56670a582f2c52176f8d4fd282911b72669bf2c
Koroleva Irina <korolevai@gmail.com>
2022-04-23 12:58:33 +0300

    added bloks: header and hero

---
+++ weblayout-4/Euclid/js/swiper.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+  });
