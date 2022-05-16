93dc5bad36fb5c67822a8546183674b745646974
Koroleva Irina <korolevai@gmail.com>
2022-05-16 15:59:02 +0300

    added footer

--- weblayout-4/Euclid/js/scripts.js
+++ weblayout-4/Euclid/js/scripts.js
@@ -9,15 +9,25 @@ const swiper = new Swiper('.swiper-container', {
   });
 
 
-document.querySelectorAll('.stages-tabs__btn').forEach(function(tabsBtn) {
-    tabsBtn.addEventListener('click', function(e) {
-        const path = e.currentTarget.dataset.path;
+document.querySelectorAll('.stages-tabs__btn').forEach(tabsBtn =>
+    tabsBtn.addEventListener('click', (e) => {
+            const path = e.currentTarget.dataset.path;
 
-    document.querySelectorAll('.stages-tabs__btn').forEach(function(btn) {
-        btn.classList.remove('stages-tabs__btn--active')});
-        e.currentTarget.classList.add('stages-tabs__btn--active');
+            document.querySelectorAll('.stages-tabs__btn').forEach((btn) => {
+                btn.classList.remove('stages-tabs__btn--active');
+            });
+            e.currentTarget.classList.add('stages-tabs__btn--active');
 
-    document.querySelectorAll('.stages__tab-item').forEach(function(tabsBtn) {
-        tabsBtn.classList.remove('stages__tab-item--active')});
-        document.querySelector(`[data-target="${path}"]`).classList.add('stages__tab-item--active');
-    }); });
+            document.querySelectorAll('.stages__tab-item').forEach((tabsBtn) => {
+                tabsBtn.classList.remove('stages__tab-item--active');
+            });
+            document.querySelector(`[data-target="${path}"]`).classList.add('stages__tab-item--active');
+        })
+);
+
+
+$(".faq-accordion").accordion ({
+    heightStyle: "content",
+    collapsible: true,
+    active: false
+  });
